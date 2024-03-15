fe9b1288443189d821784b27696356abc4411908
CarlosDanielCastilloBanda <158379780+CarlosDanielCastilloBanda@users.noreply.github.com>
2024-03-14 18:25:27 -0600

    Add files via upload

---
+++ EVA2_8_CLASES_ABSTRACTAS/src/eva2_8_clases_abstractas/Estudiante.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva2_8_clases_abstractas;
+
+/**
+ *
+ * @author invitado
+ */
+public class Estudiante extends Persona{
+    private String noControl;
+    
+    public estudiante(){
+        super();
+        this.noControl = noControl;
+        
+        
+    }
+    
+}
